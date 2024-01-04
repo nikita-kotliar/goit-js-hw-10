52b9439fcd31ea68911d60b0f55662974c1e0e6e
nikita-kotliar <knr200710@gmail.com>
2024-01-04 13:05:15 +0200

    push

---
+++ src/js/1-timer.js
@@ -0,0 +1,15 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+let userSelectedDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    console.log(selectedDates[0]);
+  },
+};
